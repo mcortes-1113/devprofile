2165d81bb7d3480b2deda6e44e37a78638a50b64
Mario Cortes <mcortes.1113@gmail.com>
2019-12-02 21:18:17 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var fs = require('fs');
+
+//create prompts for favorite color and username
+
+//variables to hold user responses
+
+//build url with username variable for github api
+
+//variables to hold api response
+
+
+const questions = [
+    `what is your github username?`,
+    `what is your favorite color?`  
+];
+
+function writeToFile(fileName, data) {
+
+// fs.appendFile("log.txt", process.argv[2] + '\n', function(err) {
+
+//   if (err) {
+//     console.log(err);
+//   }
+//   else {
+//     console.log("Commit logged!");
+//   }
+
+};
+
+ function init() {
+
+}
+
+// init();
+
+// var color = process.argv[2];
+
