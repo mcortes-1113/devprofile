ad10657b9398ae9c1b4a9026da976e2c1acf274f
Mario Cortes <mcortes.1113@gmail.com>
2019-12-02 22:29:06 -0600

    built functions to get user input and get data from github api

--- index.js
+++ index.js
@@ -1,15 +1,96 @@
 var fs = require('fs');
 var inquirer = require('inquirer');
-
-//create prompts for favorite color and username.
-
-
+var axios = require("axios");
 
 //variables to hold user responses
+var username;
+var color;
 
 //build url with username variable for github api
+var urlBase = 'https://api.github.com/users/';
 
 //variables to hold api response
+
+var imageSrc;
+var login;
+// * Links to the following:
+//   * User location via Google Maps
+//   * User GitHub profile
+//   * User blog
+var Bio;
+var repoCount;
+var followers;             
+var stars;
+var following;
+
+function printVariables() {
+    console.log(username);
+    console.log(color);
+    console.log(url);
+}
+
+function getData() {
+      axios.get(url).then(function(res) {
+    imageSrc = res.data.avatar_url;
+    login = res.data.login;
+    bio = res.data.bio;
+    repoCount = res.data.public_repos;
+    followers = res.data.followers;
+    stars = res.data.public_gists;
+    following = res.data.following;
+    console.log(imageSrc);
+    console.log(login);
+    console.log(bio);
+    console.log(repoCount);
+    console.log(followers);
+    console.log(stars);
+    console.log(following);
+    });
+}
+
+//create prompts for favorite color and username.
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your github user name?",
+      name: "username"
+    },
+    {
+      type: "input",
+      message: "What is your favorite color?",
+      name: "color"
+    }
+  ])
+  .then(function(response) {
+    username = response.username;
+    color = response.color;
+    url = urlBase + username;
+    getData();
+  });
+
+  //---------------------------------------------------------------------------------
+
+//   const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+
+//   axios.get(queryUrl).then(function(res) {
+//     const repoNames = res.data.map(function(repo) {
+//       return repo.name;
+//     });
+
+//     const repoNamesStr = repoNames.join("\n");
+
+  //---------------------------------------------------------------
+
+
+//     fs.writeFile("repos.txt", repoNamesStr, function(err) {
+//       if (err) {
+//         throw err;
+//       }
+
+//       console.log(`Saved ${repoNames.length} repos`);
+//     });
+//   });
 
 
 const questions = [
@@ -36,5 +117,4 @@ function writeToFile(fileName, data) {
 
 // init();
 
-// var color = process.argv[2];
 
